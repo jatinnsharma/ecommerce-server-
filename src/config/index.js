ff665201ea70cca8539486bae9fada51a89f04aa
jatin <jatinsharma2231@gmail.com>
2023-12-01 15:47:35 +0530

    forget password functionality added

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,9 @@ dotenv.config()
 
 const config ={
     PORT:process.env.PORT || 8000,
-    MONGO_URL: process.env.MONGO_URI
+    MONGO_URL: process.env.MONGO_URI,
+    JWT_SECRET:process.env.JWT_SECRET,
+    JWT_EXPIRY:process.env.JWT_EXPIRY
 }
-
 
 export default config;
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose'
 import AuthRoles from '../utils/authRoles'
 import bcrypt from "bcryptjs"
+import JWT from 'jsonwebtoken'
+import config from '../config/index.js'
+import crypto from 'crypto'
 
 const userSchema = new mongoose.Schema(
     {
@@ -37,9 +40,36 @@ userSchema.pre("save",async function(next){
 })
 
 userSchema.methods = {
+
     //compare password
     comparePassword: async function(enteredPassword){
         return await bcrypt.compare(enteredPassword, this.password)
+    },
+
+    // generate JWT token 
+    getJWTtoken : function(){
+        JWT.sign({
+            _id:this._id, role:this.role
+        },
+        config.JWT_SECRET
+        ,{
+            expiresIn:config.JWT_EXPIRY
+        })
+    },
+
+    // geneate forgot password token 
+    generateForgotPasswordToken:function(){
+      const forgotToken= crypto.randomBytes(20).toString('hex')
+      this.forgotPasswordToken = crypto
+       .createHash('sha256')
+       .update(forgotToken)
+       .digest("hex")
+
+    // this for token to expire 
+    this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000 
+
+    return forgotToken;
+
     }
 }
 
