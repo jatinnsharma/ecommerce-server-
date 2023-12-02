7735497f6caffd33503d28c4011105b75012e46d
jatin <jatinsharma2231@gmail.com>
2023-12-02 13:27:01 +0530

    user logged in and out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,19 @@ import User from "../models/user.schema.js"
 import asyncHandler from "../service/asyncHandler"
 import CustomError from "../utils/CustomError"
 
-export const signup = asyncHandler(async (req,res)=>{
+export const cookieOptions = {
+    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    httpOnly: true
+}
+
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @returns User Object
+ ******************************************************/
+
+export const signUp = asyncHandler(async (req,res)=>{
     // get data from user 
     const {name , email , password} = req.body 
 
@@ -38,5 +50,51 @@ export const signup = asyncHandler(async (req,res)=>{
         success:true,
         token,
         user,
+    })
+})
+
+
+
+export const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    //validation
+    if (!email || !password) {
+        throw new CustomError("PLease fill all details", 400)
+    }
+
+    const user = User.findOne({email}).select("+password")
+
+    if (!user) {
+        throw new CustomError("Invalid credentials", 400)
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if (isPasswordMatched) {
+        const token = user.getJWTtoken()
+        user.password = undefined
+        res.cookie("token", token, cookieOptions)
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+
+    throw new CustomError("Password is incorrect", 400)
+})
+
+
+
+export const logout = asyncHandler(async (req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Logged Out'
     })
 })
